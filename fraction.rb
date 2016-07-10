aaf9ede39a46b5e9768dd8a923bc225eb2d39191
atamannn <atamanyk@gmail.com>
2016-07-10 11:03:40 +0300

    Update fraction.rb

--- fraction.rb
+++ fraction.rb
@@ -1,64 +1,51 @@
 class Fraction
   def initialize(numerator, denominator)
-    if ! numerator.is_a?(Integer) && denominator.is_a?(Integer)
-      # TODO: Proper execption for this issue
-      # TODO: Handle case when one or both parameters are not int but could be easily transformed to int
-      raise 'these are supposed to be numbers: '+numerator.inspect+' '+ denominator.inspect
-    end
     @numerator = numerator
     @denominator = denominator
   end
-
   def numerator
     @numerator
   end
-
   def denominator
     @denominator
   end
-
-  def simplify
-
+  def simple
+    a = @numerator
+    b = @denominator
+    while (a != 0) && (b != 0)
+      if a > b
+        a = a % b
+      else
+        b = b % a
+      end
+    end
+    devider = a + b
 =begin
-     if @numerator > @denominator then b=@denominator else b=@numerator
-    end
-    end
-
-    while b>1
-     if (@numerator%b == 0 and @denominator%b == 0)
-       @denominator = @denominator /b
-       @numerator = @numerator /b
-     end
-      b=b-1
+    @numerator = @numerator/devider    # вот это другое дело!
+    @denominator = @denominator/devider
 =end
-    Fraction.new(@numerator / @numerator.gcd(@denominator), @denominator / @denominator.gcd(@numerator))
+    Fraction.new(@numerator = @numerator/devider, @denominator = @denominator/devider) #непрозрачный способ!!!
   end
-
   def view
-      puts "#{@numerator}/#{@denominator}"
+    "#{@numerator}/#{@denominator}"
   end
-
-  def +(a)
-    Fraction.new(@numerator*a.denominator+a.numerator*@denominator, @denominator*a.denominator).simplify
+  def *(b)
+    Fraction.new(@numerator * b.numerator, @denominator * b.denominator)
   end
-
-  def *(a)
-    Fraction.new(@numerator = @numerator*a.numerator, @denominator = @denominator*a.denominator).simplify
+  def /(b)
+    Fraction.new(@numerator * b.denominator, @denominator * b.numerator)
   end
-
-  def /(a)
-    Fraction.new(@numerator*a.denominator, @denominator*a.numerator).simplify
+  def -(b)
+    Fraction.new(@numerator*b.denominator-b.numerator*@denominator, @denominator*b.denominator)
   end
-
-  def -(a)
-    simplify
-    a.simplify
-    Fraction.new(@numerator*a.denominator-a.numerator*@denominator, @denominator*a.denominator).simplify
+  def +(b)
+    Fraction.new(@numerator*b.denominator+b.numerator*@denominator, @denominator*b.denominator)
   end
 end
 
+firstFr = Fraction.new(2, 3)
+secondFr = Fraction.new(1, 2)
+firstFr.simple
+secondFr.simple
 
-fraction1 = Fraction.new(9,12)
-fraction2 = Fraction.new(221,663)
-
-(fraction1-fraction2).view
+puts (firstFr + secondFr).simple.view
