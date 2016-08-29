16848a1d23386dab12dd698a3f7ad12cbb58a532
anna <foraccs@i.ua>
2016-08-29 13:42:17 +0300

    second try

--- lesson2/hometask.rb
+++ lesson2/hometask.rb
@@ -1,16 +1,21 @@
 module M
   module SingletonMethods
     def singleton_method
-      p "singleton method"
+      p Foo.singleton_methods
+    end
+  end
+  module InstanceMethods
+    def instance_method
+      p Foo.instance_methods - Object.instance_methods
     end
   end
 
-  def instance_method
-    p "instance method"
-  end
 
   def self.included(base)
     base.extend SingletonMethods
+    base.include InstanceMethods
+    Foo.singleton_method
+    Foo.new.instance_method
   end
 end
 
@@ -18,27 +23,4 @@ class Foo
   include M
 end
 
-Foo.singleton_method
-Foo.new.instance_method
 
-
-##second variant##
-#
-# module M
-#
-#
-#   def self.included(base)
-#     base.extend(self)
-#   end
-#
-#   def future_either_instance_or_singleton_method
-#   end
-#
-# end
-#
-# class Foo
-#   include M
-# end
-#
-# p Foo.instance_methods - Object.instance_methods
-# p Foo.singleton_methods
