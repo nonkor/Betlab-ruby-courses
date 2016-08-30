b488be1ec7aea7eaf689225d1f6ad4f0050cfc59
mariia.gorobets <mariia.gorobets@betlab.com>
2016-08-30 12:13:43 +0300

    Minor improvements

--- lesson2/hometask.rb
+++ lesson2/hometask.rb
@@ -5,17 +5,17 @@ module M
     end
   end
 
-  def self.included(base)
-    base.extend(MetaclassMethods)
-  end
-
   def instance_method_of_Foo
   end
+
+  def self.included(base)
+    base.extend(MetaclassMethods)
+    puts Foo.instance_methods - Object.instance_methods
+    puts Foo.singleton_methods - Object.singleton_methods
+  end
+
 end
 
 class Foo
   include M
 end
-
-puts Foo.instance_methods - Object.instance_methods
-puts Foo.singleton_methods - Object.singleton_methods
