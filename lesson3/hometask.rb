32367c6d56651aa30d7b4b0e2805540ac0a19d74
Vladimir Bosyi <bosyi.vladimir@gmail.com>
2016-09-07 21:28:45 +0300

    cosmetic fixes

--- lesson3/hometask.rb
+++ lesson3/hometask.rb
@@ -1,6 +1,6 @@
 module MyEach
 
-  # as far we use only Array and Hash i let myself
+  # as far we are using only Array and Hash i let myself
   # make conclusion if not class is not Array, than class is Hash
 
   def my_each
@@ -35,4 +35,4 @@ end
 
 {:wow => 'some', :wow1 => 'some1'}.my_each { |k, v| puts "key = #{k}, val = #{v}" }
 #key = wow, val = some
-#key = wow1, val = some1#
+#key = wow1, val = some1
