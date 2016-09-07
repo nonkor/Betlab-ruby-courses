a165ebce90eff36db93ece7b57e6293abbdc235e
anna <foraccs@i.ua>
2016-09-07 12:24:29 +0300

    third hometask

---
+++ lesson3/hometask3.rb
@@ -0,0 +1,23 @@
+module MyEach
+  def self.my_each
+    while true
+      yield
+    end
+  end
+
+  my_each do |i|
+    p i
+  end
+
+end
+
+class Array
+  include MyEach
+
+  [1, 2, 3].my_each
+end
+
+class Hash
+  include MyEach
+  {1 => '1', 2 => '2'}
+end
