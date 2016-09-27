491e90668a2fb8318738b75e6bf8a0a01d184308
anna <foraccs@i.ua>
2016-09-27 22:00:30 +0300

    fix

--- lesson4/hometask.rb
+++ lesson4/hometask.rb
@@ -4,8 +4,8 @@ class LazyHash < Hash
     self[method_name] = fetch(method_name).call
   end
 
-  def respond_to_missing?(method_name)
-    self[method_name] = fetch(method_name).call
+  def respond_to_missing?(method_name, include_private = false)
+    self[method_name] || super
   end
 
   def lazy? method_name
@@ -20,4 +20,6 @@ a[:b] = Proc.new { 12 }
  puts a.lazy? :b
  puts a.b
  puts a.lazy? :b
+
+
 
