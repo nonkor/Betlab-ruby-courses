3e6441aefa925b58b383da20f6e20815d18e349e
anna <foraccs@i.ua>
2016-09-28 10:16:26 +0300

    respond_to_missing

--- lesson4/hometask.rb
+++ lesson4/hometask.rb
@@ -4,20 +4,27 @@ class LazyHash < Hash
     self[method_name] = fetch(method_name).call
   end
 
-  def respond_to_missing?(method_name)
-    self[method_name] = fetch(method_name).call
+  def respond_to_missing?(method_name, include_private = false)
+    if fetch(method_name)
+      true
+    else
+      false
+    end
   end
 
   def lazy? method_name
-     fetch(method_name).instance_of? Proc
-    end
+    fetch(method_name).instance_of? Proc
+  end
+
 end
 
 
 a = LazyHash.new
 a[:b] = Proc.new { 12 }
- puts a[:b]
- puts a.lazy? :b
- puts a.b
- puts a.lazy? :b
+puts a[:b]
+puts a.lazy? :b
+puts a.b
+puts a.lazy? :b
+
+
 
