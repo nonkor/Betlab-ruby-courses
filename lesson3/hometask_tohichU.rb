c747894412de647e02b5577f23ef8b11f974ae5a
tohich <anton.uhlenko@betlab.com>
2016-10-23 22:31:53 +0300

    a.uhlenko hometask2

---
+++ lesson3/hometask_tohichU.rb
@@ -0,0 +1,25 @@
+# 1. создайте бранч hometask2_<githubname> из мастера
+# 2. создайте в папке lesson2  файл hometask_<githubname>.rb
+
+module MyEach
+
+  def my_each
+    return self.to_enum(:my_each) unless block_given?
+    clone = self.clone
+    while item = clone.shift
+      yield(item)
+    end
+  end
+end
+
+class Array
+  include MyEach
+end
+
+class Hash
+  include MyEach
+end
+
+[1, 2, 3, 4].my_each { |i| puts "Array item: #{i}" }
+
+{a => 1, b => 2}.my_each { |key, value| puts "Hash key: #{key}: Value #{value}" }
