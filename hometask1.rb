a029b8323182376baa3f3524b7a983335818107c
tetyana.karpova <tetyana.karpova@betlab.com>
2016-11-10 10:44:10 +0200

    add comments

--- hometask1.rb
+++ hometask1.rb
@@ -10,18 +10,17 @@ class Foo
   end
 end
 
-uniq_instans = Foo.new
+uniq_instans = Foo.new          # first instance of Foo class
 
-
-def uniq_instans.my_instance
+def uniq_instans.my_instance    # singleton method of uniq_instans - instance of Foo class
   :uniq
 end
 
-new_instans = Foo.new
-next_instans = Foo.new
-foo = Foo.new
+new_instans = Foo.new           # second instance of Foo class
+next_instans = Foo.new          # third instance of Foo class
+foo = Foo.new                   # fourth instance of Foo class
 
-puts uniq_instans.my_instance
-puts new_instans.my_instance
-puts next_instans.my_instance
-puts foo.my_instance
+puts uniq_instans.my_instance   # => :uniq
+puts new_instans.my_instance    # => :even
+puts next_instans.my_instance   # => :odd
+puts foo.my_instance            # => :even
