a598e751f3f4e1fc4f07eb274d9d867ff0545c19
tetyana.karpova <tetyana.karpova@betlab.com>
2016-11-14 16:16:08 +0200

    Add hometask2 from TetyanaKarpova
    Add hometask2 from TetyanaKarpova
    
    Add hometask2 from TetyanaKarpova

--- lesson2/hometask_TetyanaKarpova.rb
+++ lesson2/hometask_TetyanaKarpova.rb
@@ -6,19 +6,15 @@ module M
   end
 
   module MInstanceMethods
-
     def m_instance_method
       puts 'Instance method'
     end
-
   end
 
   module MClassMethods
-
     def m_class_method
       puts 'Class method'
     end
-
   end
 end
 
@@ -28,7 +24,5 @@ end
 
 foo = Foo.new
 
-Foo.m_class_method    # => "Class method"
-foo.m_instance_method # => "Instance method"
-Foo.m_instance_method # => undefined method `m_instance_method' for Foo:Class (NoMethodError)
-foo.m_class_method    # => undefined method `m_class_method' for #<Foo:0x00000003024c90>
+puts Foo.singleton_methods              # => m_class_method
+puts Foo.new.methods - Object.methods   # => m_instance_method
