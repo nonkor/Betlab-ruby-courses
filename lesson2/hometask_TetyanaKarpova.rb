60ae7dfb66866210791edcd2fde43127700aa75a
tetyana.karpova <tetyana.karpova@betlab.com>
2016-11-17 12:29:02 +0200

    hometask2 from TetyanaKarpova

--- lesson2/hometask_TetyanaKarpova.rb
+++ lesson2/hometask_TetyanaKarpova.rb
@@ -3,8 +3,8 @@ module M
   def self.included(base)
     base.include M::MInstanceMethods
     base.extend M::MClassMethods
-    puts base.singleton_methods              # => m_class_method
-    puts base.new.methods - Object.methods   # => m_instance_method
+    puts base.singleton_methods                    # => m_class_method
+    puts base.instance_methods  - Object.methods   # => m_instance_method
   end
 
   module MInstanceMethods
@@ -23,5 +23,3 @@ end
 class Foo
   include M
 end
-
-foo = Foo.new
