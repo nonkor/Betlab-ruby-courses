f56503c1c7209fb52ea39b6203d9e5b653c4ee2d
zenameless <karpenkoconstantin@gmail.com>
2016-11-17 22:34:33 +0200

    Add hometask2 from zenameless

--- lesson2/zenameless1_hometask2.rb
+++ lesson2/zenameless1_hometask2.rb
@@ -1,25 +1,35 @@
 module M
 
   def self.included(base)
-    base.include InstanceMethods
-    base.extend ClassMethods
+    base.include M::InstanceMethods
+    base.extend M::ClassMethods
+    puts "Class methods:\t#{base.singleton_methods}"
+    puts "Instance methods:\t#{base.instance_methods - Object.methods}"
   end
 
   module ClassMethods
+
     def first_method
       puts __method__
     end
   end
 
   module InstanceMethods
+
     def second_method
       puts __method__
     end
+    
   end
 
 end
 
-Foo = Class.new
-Foo.include M
-puts "Class methods:\t#{Foo.singleton_methods}"
-puts "Instance methods:\t#{Foo.new.methods - Object.methods}"
+class Foo
+
+  include M
+
+end
+
+
+foo = Foo.new
+
