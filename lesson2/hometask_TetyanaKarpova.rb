84ba7a246676f3efaecde20a9011432a32be1c8e
TetyanaKarpova <tasya89@ukr.net>
2016-11-22 16:42:06 +0200

    Add hometask2 from TetyanaKarpova (#47)

---
+++ lesson2/hometask_TetyanaKarpova.rb
@@ -0,0 +1,25 @@
+module M
+
+  def self.included(base)
+    base.include M::MInstanceMethods
+    base.extend M::MClassMethods
+    puts base.singleton_methods                    # => m_class_method
+    puts base.instance_methods  - Object.methods   # => m_instance_method
+  end
+
+  module MInstanceMethods
+    def m_instance_method
+      puts 'Instance method'
+    end
+  end
+
+  module MClassMethods
+    def m_class_method
+      puts 'Class method'
+    end
+  end
+end
+
+class Foo
+  include M
+end
