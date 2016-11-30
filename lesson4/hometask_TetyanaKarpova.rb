8ac32b41e308bfd51ba0788587c07cf2d225896f
tetyana.karpova <tetyana.karpova@betlab.com>
2016-11-30 12:37:49 +0200

    hometask4 from TetyanaKarpova

--- lesson4/hometask_TetyanaKarpova.rb
+++ lesson4/hometask_TetyanaKarpova.rb
@@ -4,21 +4,21 @@ class LazyHash < Hash
   end
 
   def method_missing(method_name, *args)
-    if fetch(method_name).is_a? Proc
-      self[method_name] = fetch(method_name).call
+    if self.has_key?(method_name)
+      fetch(method_name).is_a?(Proc) ? self[method_name] = fetch(method_name).call : self[method_name]
     else
       super
     end
   end
 
   def respond_to_missing?(method_name, include_private = false)
-    fetch(method_name).is_a? Proc || super
+    self.has_key?(method_name) || super
   end
 end
 
 lazy_h = LazyHash.new
 lazy_h[:k] = proc {"!!!!!"}
-puts lazy_h[:k].is_a? Proc # => true
-puts lazy_h.lazy? :k       # => true
-puts lazy_h.k              # => !!!!!
-puts lazy_h[:k]            # => !!!!!
+puts lazy_h[:k]       # =>
+puts lazy_h.lazy? :k  # => true
+puts lazy_h.k         # => !!!!!
+puts lazy_h[:k]       # => !!!!!
