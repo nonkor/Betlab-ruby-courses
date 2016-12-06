31ab17fcd898ee49a3782250b9e4390d2dc27fa5
tetyana.karpova <tetyana.karpova@betlab.com>
2016-12-06 12:13:13 +0200

    hometask5 from TetyanaKarpova

--- lesson5/hometask_TetyanaKarpova.rb
+++ lesson5/hometask_TetyanaKarpova.rb
@@ -1,12 +1,21 @@
 class LazyHash < Hash
-  def lazy?(method_name)
-    fetch(method_name).is_a? Proc
+  def lazy?(key)
+    fetch(key).is_a? Proc
   end
 
   def []=(key, value)
     unless self.has_key? key
       define_singleton_method(key) do
-        value.is_a?(Proc) ? self[key] = fetch(key).call : value
+        value.is_a?(Proc) ? self[key] = value.call : value
+      end
+    end
+    super
+  end
+
+  def self.[](*params)
+    super.each_pair do |key, value|
+      define_method(key) do
+        value.is_a?(Proc) ? self[key] = value.call : value
       end
     end
     super
@@ -15,8 +24,13 @@ end
 
 lazy_h = LazyHash.new
 lazy_h[:k] = proc {"!!!!!"}
-puts lazy_h[:k].is_a?(Proc)   # => true
-puts lazy_h.lazy? :k          # => true
-puts lazy_h.k                 # => !!!!!
-puts lazy_h[:k]               # => !!!!!
-puts lazy_h.lazy? :k          # => false
+p lazy_h[:k].is_a?(Proc)             # => true
+p lazy_h.lazy? :k                    # => true
+p lazy_h.k                           # => "!!!!!"
+p lazy_h[:k]                         # => "!!!!!"
+p lazy_h.lazy? :k                    # => false
+h = LazyHash[:a, proc {"!"}, :b, 2]
+p h.b                                # => 2
+p h.lazy? :a                         # => true
+p h.a                                # => "!"
+p h.lazy? :a                         # => false
